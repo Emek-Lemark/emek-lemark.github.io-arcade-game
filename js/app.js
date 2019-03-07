995b5fec7a037e5b86dc45ac86479507ba6f6e9e
Emeka Onyenwe <emeka.onyenwe@gmail.com>
2019-03-07 07:24:07 +0100

    feat: updating gameplay

--- js/app.js
+++ js/app.js
@@ -1,67 +1,68 @@
 // Declare Glabal Variables
 const tileWidth = 101;
 const tileHeight = 85;
-const canvasHeight = 1000;
+const canvasHeight = 985;
 const canvasWidth = 1305;
 const playerWidth = 65;
 const playerHeight = 50;
 const numColumns = 13;
 const maxY = (tileHeight * 10) + (tileHeight / 2);
-
-
+// Variable to store collectible images
+const collectibleSprite = [
+    'images/gem-blue.png',
+    'images/gem-green.png',
+    'images/gem-orange.png'
+];
 
 // Enemies our player must avoid
 class Enemy {
-    constructor(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    constructor() {
+// Variables applied to each of our instances go here,
+// we've provided one for you to get started
+// The image/sprite for our enemies, this uses
+// a helper we've provided to easily load images
+    this.sprite = 'images/bug.png';
     this.x = -10;
-    this.width = 40;
-    this.height = 40;
     this.y = (Math.floor((Math.random() * 8)) * tileHeight) + tileHeight + (tileHeight / 2);
     this.speed = Math.max(Math.random(), 0.5);
     this.step = 450;
+    this.width = 100;
+    this.height = 40;
 }
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
     update(dt) {
-        this.x = (this.x + (dt * this.speed * this.step)) % (canvasWidth + this.width);
-
-        if(this.x > (canvasWidth)) {
+    this.x = (this.x + (dt * this.speed * this.step)) % (canvasWidth + this.width);
+    if(this.x > (canvasWidth)) {
         this.y = (Math.floor((Math.random() * 8)) * tileHeight) + tileHeight + (tileHeight / 2);
         this.speed = Math.max(Math.random(), 0.5);
     }
-
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+// You should multiply any movement by the dt parameter
+// which will ensure the game runs at the same speed for
+// all computers.
 }
-
 // Draw the enemy on the screen, required method for game
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 class Player {
-    constructor(x, y) {
+    constructor() {
     this.sprite = 'images/char-boy.png';
-// Place player in the middle of the sixth tile from left horizontally
-    this.x = (tileWidth * 5) + (tileWidth / 2) - (playerWidth / 2);
+// Place player in the middle of the seventh tile from left horizontally
+    this.x = (tileWidth * 6) + (tileWidth / 2) - (playerWidth / 2);
 // Place player in the middle of the tenth tile from top vertically
-    this.y = (tileHeight * 9) + (tileHeight / 2) - (playerHeight / 2);
+    this.y = (tileHeight * 10) + (tileHeight / 2) - (playerHeight / 2);
 // moveY and moveX for move direction, initially set to zero
     this.moveY = 0;
     this.moveX = 0;
     this.width = playerWidth;
-    this.height = 85;
-    }
+    this.height = 30;
+
+}
 
     update(dt) {
     // Calculate the next X position based on direction
@@ -71,7 +72,7 @@ class Player {
         this.x = nextX;
     }
     // Calculate the next Y position based on direction
-    var nextY = this.y + (this.moveY * tileHeight);
+    let nextY = this.y + (this.moveY * tileHeight);
     // Update position only if within canvas bounds
     if (nextY <= maxY && nextY >= 0 ) {
         this.y = nextY;
@@ -79,8 +80,7 @@ class Player {
     // After updating position, reset direction to zero
     this.moveY = 0;
     this.moveX = 0;
-
-    }
+}
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -104,67 +104,53 @@ class Player {
             this.moveX = 0;
             }
         }
-
-    }
-
-
-// variable to store collectible images
-const collectibleSprite = [
-    'images/gem-blue.png',
-    'images/gem-green.png',
-    'images/gem-orange.png'
-
-];
-// collectible class
+}
+// Collectible class
 class Collectible {
     constructor(collectibleType) {
     // X and Y position of the gem
     this.x = (Math.floor((Math.random() * 8)) * tileWidth) + (tileWidth * 1.5);
     this.y = (Math.floor((Math.random() * 8)) * tileHeight) + (tileHeight * 1.5);
-    this.width = 50;
-    this.height = 55;
+    this.width = 60;
+    this.height = 60;
 
-    //50 points scored for blue, 100 for green and 150 for orange gem.
-    this.point = (collectibleType + 1) * 50;
+    // 50 points scored for blue, 100 for green and 150 for orange gem.
+    this.score = (collectibleType + 1) * 50;
     this.sprite = collectibleSprite[collectibleType];
-
-
     }
+
     update(dt) {
     // Change collectible position 
     if (Math.random() < 0.001) {
-        this.x = (Math.floor((Math.random() * 7)) * tileWidth) + (tileWidth * 1.5);
-        this.y = (Math.floor((Math.random() * 7)) * tileHeight) + (tileHeight * 1.5);
+        this.x = (Math.floor((Math.random() * 8)) * tileWidth) + (tileWidth * 1.5);
+        this.y = (Math.floor((Math.random() * 8)) * tileHeight) + (tileHeight * 1.5);
     }
 }
 
-    render() {
+   render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);    
 
     }
 
 }
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 let allEnemies = [];
-let player = new Player(200, 390);
+let player = new Player();
 
 // Initiate enemies, might make number of enemies dynamic in future.
 for (let i = 0; i < 9; i++) {
     allEnemies.push(new Enemy());
 }
 // Number of collectibles, anywhere between 5 to 9
-const numberOfCollectibles = Math.max(Math.floor(Math.random() * 10), 5);
+let numberOfCollectibles = Math.max(Math.floor(Math.random() * 10), 5);
 let allCollectibles = [];
 for (let i = 0; i < numberOfCollectibles; i++) {
 // Generate collectible type randomly
-let collectibleType = Math.floor((Math.random() * 3));
-allCollectibles.push(new Collectible(collectibleType));
+    let collectibleType = Math.floor((Math.random() * 3));
+    allCollectibles.push(new Collectible(collectibleType));
 }
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -177,3 +163,11 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Prevent the window from scrolling on arrow key press
+window.addEventListener('keydown', function(e) {
+    // keycodes for the space bar and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
--- js/engine.js
+++ js/engine.js
@@ -12,24 +12,46 @@
  * This engine makes the canvas' context (ctx) object globally available to make
  * writing app.js a little simpler to work with.
  */
-
-var Engine = (function(global) {
+const begin =function(){ //I want to start after click in button Start
+const ChoiceStart = document.getElementById("startpage");
+const Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
      * set the canvas element's height/width and add it to the DOM.
      */
-    var doc = global.document,
+    let doc = global.document,
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         lastTime;
-        var numColumns = 13;
+        const numColumns = 13;
+        const canvasHeight = 985;
+        const canvasWidth = 1305;
 
-    canvas.width = 1200;
-    canvas.height = 1000;
+    let lifeLeft;
+    let scores;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
     doc.body.appendChild(canvas);
 
-    /* This function serves as the kickoff point for the game loop itself
+    // Code to play the background music, uses Howler.js for playing sound
+    var backgroundMusic = new Howl({
+    src: ['sound/zelda-chill.ogg', 'sound/zelda-chill.mp3'],
+    autoplay: false,
+    loop: true,
+    volume: 0.2,
+    });
+    // Start the background music
+    backgroundMusic.play();
+    // This defines the sound played when collecting gems
+    var collectibleSound = new Howl({
+    src: ['sound/definite.mp3'],
+    autoplay: false,
+    loop: false,
+    volume: 1,
+  });
+
+  /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
      */
     function main() {
@@ -58,17 +80,17 @@ var Engine = (function(global) {
          */
         win.requestAnimationFrame(main);
     }
-
     /* This function does some initial setup that should only occur once,
      * particularly setting the lastTime variable that is required for the
      * game loop.
      */
     function init() {
+        lifeLeft = 5;
+        scores = 0;
         reset();
         lastTime = Date.now();
         main();
     }
-
     /* This function is called by main (our game loop) and itself calls all
      * of the functions which may need to update entity's data. Based on how
      * you implement your collision detection (when two entities occupy the
@@ -80,9 +102,31 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisionsWithEnemy();
+        checkScores();
     }
-
+    /* This function is called by the update method. It checks if 
+     * the player has reached the water or has collected any gems
+     */
+    function checkScores() {
+    // If player has reached the last brick then add scores and reset player
+    if (player.y <= 60) {
+        scores += 200;
+        resetPlayer();
+    }
+    // Check all gems if player is within collision distance
+    allCollectibles.forEach(function(collectible, index, array) {
+      if (checkCollisions(collectible, player)) {
+    // Player is within collision distance of collectible
+        console.log("Collected");
+        scores += collectible.score;
+    //Play sound when player collects gem
+        collectibleSound.play();
+    // Player has collected this gem, so remove it from array
+        array.splice(index, 1);
+            }
+        });
+    }
     /* This is called by the update function and loops through all of the
      * objects within your allEnemies array as defined in app.js and calls
      * their update() methods. It will then call the update function for your
@@ -96,10 +140,39 @@ var Engine = (function(global) {
         });
         player.update();
         allCollectibles.forEach(function(collectible) {
-            collectible.update(dt);
-    });
+        collectible.update(dt);
+        });
     }
-
+    /* This function is called by the update function and loops through
+     * all the enemies to find out if player has collided with any one 
+     * of them.
+     */
+    function checkCollisionsWithEnemy() {
+        allEnemies.forEach(function(enemy) {
+         if (checkCollisions(enemy, player)) {
+            lifeLeft -= 1;
+         if (lifeLeft === 0) {
+          init();
+        }
+        resetPlayer();
+            }
+        });
+    }
+    /* This function checks collision between two objects obj1 and obj2 and 
+     * returns a boolean value. This function is used by checkCollisionWithEnemy and
+     * checkScores function.
+     */
+    function checkCollisions(object1, object2) {
+        if (object1.x < object2.x + object2.width &&
+            object1.x + object1.width > object2.x &&
+            object1.y < object2.y + object2.height &&
+            object1.height + object1.y > object2.y) {
+            console.log("Collision detected");
+        return true;
+    } else {
+      return false;
+        }
+    }
     /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
      * game tick (or loop of the game engine) because that's how games work -
@@ -116,19 +189,16 @@ var Engine = (function(global) {
         'images/stone-block.png', // Row 2 of 8 of stone
         'images/stone-block.png', // Row 3 of 8 of stone
         'images/stone-block.png', // Row 4 of 8 of stone
+        'images/stone-block.png', // Row 5 of 8 of stone
         'images/stone-block.png', // Row 6 of 8 of stone
         'images/stone-block.png', // Row 7 of 8 of stone
         'images/stone-block.png', // Row 8 of 8 of stone
         'images/grass-block.png', // Row 1 of 2 of grass
-        'images/grass-block.png', // Row 2 of 2 of grass
+        'images/grass-block.png' // Row 2 of 2 of grass
             ],
-            numRows = 10,
+            numRows = 11,
             numCols = numColumns,
             row, col;
-
-        // Before drawing, clear existing canvas
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-
         /* Loop through the number of rows and columns we've defined above
          * and, using the rowImages array, draw the correct image for that
          * portion of the "grid"
@@ -145,9 +215,10 @@ var Engine = (function(global) {
                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
             }
         }
-
         renderEntities();
         renderCollectibles();
+        renderLifeLeft();
+        renderScore();
     }
 
     /* This function is called by the render function and is called on each game
@@ -164,42 +235,66 @@ var Engine = (function(global) {
 
         player.render();
     }
-
-
-
-// Character Images
-    let charImages = document.querySelectorAll(".char-image");
-    for(let i = 0; i < charImages.length; i++) {
-// Set the default Character Image
-    charImages[0].classList.add("active");
-// Loop over Character Images and Change the Selected one based on a 'Click' event
-    charImages[i].addEventListener("click", function() {
-// Change the player image
-    player.sprite = this.getAttribute("data-image");
-// Remove class `active`from all character images
-    charImages.forEach(function(image) {
-    image.classList.remove("active");
-})
-// Add class `active` to the selected character image
-        this.classList.add("active");
-    });
-}
-
-
-// Function to render all the gems
+    // Function to render life left  of the player
+    function renderLifeLeft() {
+        const lifeImage = "images/heart-small.png";
+        const x = 1000;
+        const y = 60;
+        ctx.drawImage(Resources.get(lifeImage), x, y);
+        ctx.font = "32px Comic Sans MS";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.strokeStyle = "#dbdbdbd";
+        ctx.strokeText(lifeLeft, 1040, 92);
+        ctx.fillText(lifeLeft, 1040, 92);
+    }
+    // Function to render score scored by the player.
+    function renderScore() {
+        const scoreImage = "images/star-small.png";
+        const x = 1110;
+        const y = 70;
+        ctx.drawImage(Resources.get(scoreImage), x, y);
+        ctx.font = "32px Comic Sans MS";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.strokeStyle = "#dbdbdbd";
+        ctx.strokeText(scores, 1140, 95);
+        ctx.fillText(scores, 1140, 95);
+    }
+    // Function to render all the gems
     function renderCollectibles() {
-    allCollectibles.forEach(function(collectible) {
-    collectible.render();
-    });
-  }
+        allCollectibles.forEach(function(collectible) {
+        collectible.render();
+        });
+    }
     /* This function does nothing but it could have been a good place to
      * handle game reset states - maybe a new game menu or a game over screen
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
-        // noop
+        resetPlayer();
+        resetEnemies();
     }
-
+    // Function to reset player position and collectibles.
+    function resetPlayer() {
+        player = new Player();
+        resetCollectibles();
+        resetEnemies();
+    }
+    // Function to reset collectibles
+    function resetCollectibles() {
+        let numberOfCollectibles = Math.max(Math.floor(Math.random() * 10), 5);
+        allCollectibles = [];
+        for (let i = 0; i < numberOfCollectibles; i++) {
+        let collectibleType = Math.floor((Math.random() * 3));
+        allCollectibles.push(new Collectible(collectibleType));
+        }
+    }
+    // Function to reset Enemies
+     function resetEnemies() {
+        allEnemies = [];
+        for (let i = 0; i < 9; i++) {
+            allEnemies.push(new Enemy());
+        }
+    }
     /* Go ahead and load all of the images we know we're going to need to
      * draw our game level. Then set init as the callback method, so that when
      * all of these images are properly loaded our game will start.
@@ -208,21 +303,20 @@ var Engine = (function(global) {
         'images/stone-block.png',
         'images/water-block.png',
         'images/grass-block.png',
-        'images/enemy-bug.png',
+        'images/bug.png',
         'images/char-boy.png',
-        'images/char-cat-girl.png',
-        'images/char-horn-girl.png',
-        'images/char-pink-girl.png',
-        'images/char-princess-girl.png',
         'images/gem-blue.png',
         'images/gem-green.png',
         'images/gem-orange.png',
+        'images/heart-small.png',
+        "images/star-small.png"
+
     ]);
     Resources.onReady(init);
-
     /* Assign the canvas' context object to the global variable (the window
      * object when run in a browser) so that developers can use it more easily
      * from within their app.js files.
      */
     global.ctx = ctx;
-})(this);
+    ChoiceStart.style.display = 'none';
+})(this);};
